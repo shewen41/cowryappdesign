1f989ca48aa4c052dff9ca8b6045bf74422c0f09
shewen41 <habeeb_seun@yahoo.com>
2019-11-29 13:59:37 +0100

    cowryapp

---
+++ main2.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function() {
+
+	 
+	$("#indexbtn1").click(function() {
+		alert("Please Login or Register");
+	});
+	
+	$("#indexbtn2").click(function() {
+		alert("Yello");
+	});
+	
+	$("#indexbtn3").click(function() {
+		alert("Please Login or Register");
+	});
+	
+})
